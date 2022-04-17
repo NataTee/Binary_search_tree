dbc7f11434e103f12db893e968e598d2eee77d79
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-04-17 13:40:51 -0700

    added stubbed methods

--- bintree.cpp
+++ bintree.cpp
@@ -1,7 +1,113 @@
-/*
- * Your comment header here
- */
+/***************************************************************************
+Name: Natalia Ksenz
+Coding 07
+Purpose: The assignment to create a Binary tree object with Data and Node
+structures.
+
+File: bintree.cpp
+***************************************************************************/
 
 #include "bintree.h"
 
+BinTree::BinTree() {
+    root = NULL;
+    count = 0;
+}
 
+BinTree::~BinTree() {
+    clear();
+}
+
+bool BinTree::isEmpty() {
+    bool empty = true;
+    if (root != NULL) {
+        empty = false;
+    };
+    return empty;
+}
+
+int BinTree::getCount() {
+    return count;
+}
+
+bool BinTree::getRootData(Data *data) {
+    return 0;
+}
+
+void BinTree::displayTree() {
+    return;
+}
+
+void BinTree::clear() {
+    clear(root);
+}
+
+//public methods
+bool BinTree::addNode(int id, const string *info) {
+  return 0;
+}
+
+bool BinTree::removeNode(int id) {
+    return 0;
+}
+
+bool BinTree::getNode(Data*, int) {
+    return 0;
+}
+
+bool BinTree::contains(int id) {
+    return 0;
+}
+
+int BinTree::getHeight() {
+    return;
+}
+
+void BinTree::displayPreOrder() {
+    displayPreOrder(root);
+}
+
+void BinTree::displayPostOrder() {
+    displayPostOrder(root);
+}
+
+void BinTree::displayInOrder() {
+    displayInOrder(root);
+}
+
+void BinTree::clear(DataNode*) {
+
+}
+
+//overloaded methods
+bool BinTree::addNode(DataNode *newNode, DataNode **root) {
+    return 0;
+}
+
+DataNode *BinTree::removeNode(int id, DataNode *temproot) {
+    return 0;
+}
+
+bool BinTree::getNode(Data*, int, DataNode*) {
+    return 0;
+}
+
+bool BinTree::contains(int id, DataNode *temproot) {
+    return 0;
+}
+
+int BinTree::getHeight(DataNode* temproot) {
+    return;
+}
+
+void BinTree::displayPreOrder(DataNode *temproot) {
+    return;
+}
+
+void BinTree::displayPostOrder(DataNode *temproot) {
+    return;
+}
+
+void BinTree::displayInOrder(DataNode *temproot) {
+    return;
+}
--- bintree.h
+++ bintree.h
@@ -1,12 +1,67 @@
-/*
- * Your comment header here
- */
+/***************************************************************************
+Name: Natalia Ksenz
+Coding 07
+Purpose: The assignment to create a Binary tree object with Data and Node
+structures.
+
+File: bintree.h
+***************************************************************************/
 
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
 
+#include <iostream>
+#include <string>
+#include "data.h"
 
+using std::cout;
+using std::endl;
+using std::string;
 
+class BinTree {
+public:
+
+    BinTree();
+    ~BinTree();
+
+    //tree management without overloads
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+
+    //tree management with private overloads
+    void clear();
+    bool addNode(int, const string*);
+    bool removeNode(int);
+    bool getNode(Data*, int);
+    bool contains(int);
+    int getHeight();
+
+    //display functions with private overloads
+    void displayPreOrder();
+    void displayPostOrder();
+    void displayInOrder();
+
+    //doesn't need overload
+    void displayTree();
+
+private:
+
+    DataNode *root;
+    int count;
+
+    //overloaded tree management
+    void clear(DataNode*);
+    bool addNode(DataNode*, DataNode**);
+    bool removeNode(int, DataNode*);
+    bool getNode(Data*, int, DataNode*);
+    bool contains(int, DataNode*);
+    int getHeight(DataNode*);
+
+    //overloaded display functions
+    void displayPreOrder(DataNode*);
+    void displayPostOrder(DataNode*);
+    void displayInOrder(DataNode*);
+};
 
 #endif /* BINTREE_BINTREE_H */
-
